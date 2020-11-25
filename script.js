aba6660e93ef4497db5a7141b153cbdc2410040d
MiekeH <63008309+MiekeH@users.noreply.github.com>
2020-11-25 21:47:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Magic Mystic Monkeys";
+  document.getElementById("demo1").style-backgroundColor = "yellow";
+  document.getElementById("demo2").innerHTML = "List of three key Items";
+  document.getElementById("demo3").innerHTML = Date();
+  document.getElementById("demo4").innerHTML = "Hello Reader of my Career path!, Find herewith a list of my work experience";
+  
+}
+
+</script> 
