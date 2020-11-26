b480c47aea03908f6efb43596afcd2a59bf5f437
MiekeH <63008309+MiekeH@users.noreply.github.com>
2020-11-26 16:43:13 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-<script>
 function myFunction() {
-  document.getElementById("demo").innerHTML = "Magic Mystic Monkeys";
-  document.getElementById("demo1").style-backgroundColor = "yellow";
-  document.getElementById("demo2").innerHTML = "List of three key Items";
-  document.getElementById("demo3").innerHTML = Date();
-  document.getElementById("demo4").innerHTML = "Hello Reader of my Career path!, Find herewith a list of my work experience";
+  document.getElementById("demo").innerHTML = "Magic Mystic Monkeys Website";
+   
+}
+
+function myFunction1() {
+  document.getElementById("demo1").innerHTML = "Magic Mystic Monkeys";
+  
   
 }
 
-</script> 
+function myFunction2() {
+  document.getElementById("demo2").innerHTML = Date();
+  
+}
+
+function myFunction3() {
+  document.getElementById("demo3").innerHTML = "Hello This is My Picture underneath the Jacket,...";
+}
+
+function myFunction4() {
+  document.getElementById("demo4").innerHTML = "List of three key Items";
+  }
+
+function myFunction5() {
+document.getElementById("demo5").style-backgroundColor = "yellow";
+  
+}
+
+function myFunction6() {
+  document.getElementById("demo6").innerHTML = "Hello interested in my Career path: Please find herewith a list of my work experiences";
+ }
