5d4821702d82d6da87dfacddeae157cf173b6fdf
MiekeH <63008309+MiekeH@users.noreply.github.com>
2020-11-27 12:03:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function myFunction() {
-  document.getElementById("demo").innerHTML = "Magic Mystic Monkeys Website";
-   
+  document.getElementById("demo").innerHTML = "Experience in different business domains, Eagerness to Learn and Look Beyond, Empowerment to Change";
+  document.getElementById("demo1").innerHTML = "Business Finance";
+   document.getElementById("demo2").innerHTML = "MagicMonkeys";
+  document.getElementById("demo3").innerHTML = Date();
 }
 
 function myFunction1() {
